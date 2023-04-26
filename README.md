87b2a72dc9410193c79ca1f62295d08c69bdff62
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-04-26 13:52:25 +0000

    first task

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # bored_cloud_engineer
+
 A) Markdown Python script
 B) Jenkins deployment
+
+TASK 1
+------
+A bored cloud engineer comes to you and says he needs a Python script that will generate a Markdown file with suggestions for activities. The script should take a number between 1-100 as an input, which will indicate how many activity suggestions there will be. To generate the activities you should use the bored api: https://www.boredapi.com/.
+ 
+The produced Markdown file should have in the first section a table of the 1-100 activities and all details that the boredapi produces:
+Key | Type | Activity | Participants | Price | Link | Accessibility
+xxx | xxxx | xxxxxx   | xxxxxxxxxx   | xxxxx | xxx  | xxxxxxx
+xxx | xxxx | xxxxxx   | xxxxxxxxxx   | xxxxx | xxx  | xxxxxxx
+ 
+Then the second section in the Markdown file will give some insights to the activities such as:
+Most Participants: Hold a video game tournament with some friends (participants: 4)
+Duplicate Activities: [here list of activities in case there are duplicates in the list, otherwise just "-"]
+Most characters in Activity: Play football with your family and friends (characters: 42)
+ 
+In the end you will have a markdown file with those two sections and the cloud engineer will be happy!
+ 
+ 
+ 
+TASK 2
+------
+Another bored cloud engineer comes to you and asks you to deploy Jenkins for him. Only requirement is that it needs to run inside a container. You can use the official Jenkins Docker image (latest version is fine). You can either run it with Docker (option a) or if you want to totally impress the bored cloud engineer you can deploy it on Kubernetes (option b).
+ 
+Instructions:
+- Install Docker Desktop (free for private use): https://docs.docker.com/desktop/windows/install/
+- Option a:
+  - Use Jenkins Docker image: https://hub.docker.com/r/jenkins/jenkins
+  - Run the Jenkins Docker container and see if you can access the application
+- Option b:
+  - Check how to deploy Kubernetes locallly on your computer using Docker Desktop: https://docs.docker.com/desktop/kubernetes/
+  - Install Helm: https://helm.sh/docs/
+  - Use Jenkins helm chart to deploy: https://github.com/jenkinsci/helm-charts
+  - Once Jenkins is deployed see if you can access the application
+ 
+ 
+Once you have Jenkins up and running it would be great if you created a Jenkins declarative pipeline as an example for the bored cloud engineer. The pipeline stages would be as follows:
+- 1st stage does git clone on repository https://github.com/EnterpriseQualityCoding/FizzBuzzEnterpriseEdition
+- 2nd stage lists the files in that repo
+- 3rd stage sends an email with link to the build
---
+++ task_1.py
@@ -0,0 +1,71 @@
+import requests
+
+
+def get_activity_suggestions(num_activities):
+    activities = []
+    for i in range(num_activities):
+        response = requests.get('https://www.boredapi.com/api/activity')
+        activity_data = response.json()
+        activity = {
+            'key': i+1,
+            'type': activity_data['type'],
+            'activity': activity_data['activity'],
+            'participants': activity_data['participants'],
+            'price': activity_data['price'],
+            'link': activity_data['link'],
+            'accessibility': activity_data['accessibility']
+        }
+        activities.append(activity)
+    return activities
+
+
+def generate_table(activities):
+    # Create table header
+    header = '| Key | Type | Activity | Participants | Price | Link | Accessibility |\n'
+    header += '| --- | ---- | -------- | ------------ | ----- | ---- | -------------- |\n'
+
+    # Create table rows
+    rows = ''
+    for activity in activities:
+        rows += '| {} | {} | {} | {} | {} | {} | {} |\n'.format(
+            activity['key'], activity['type'], activity['activity'],
+            activity['participants'], activity['price'], activity['link'], activity['accessibility'])
+
+    return header + rows
+
+
+def generate_insights(activities):
+    # Most Participants
+    most_participants = max(activities, key=lambda x: x['participants'])
+    most_participants_text = 'Most Participants: {} (participants: {})\n'.format(
+        most_participants['activity'], most_participants['participants'])
+
+    # Duplicate Activities
+    activity_names = [activity['activity'] for activity in activities]
+    duplicate_activities = set([x for x in activity_names if activity_names.count(x) > 1])
+    if duplicate_activities:
+        duplicate_activities_text = 'Duplicate Activities: {}\n'.format('\n'.join(duplicate_activities))
+    else:
+        duplicate_activities_text = 'Duplicate Activities: -\n'
+
+    # Most Characters in Activity
+    most_characters = max(activities, key=lambda x: len(x['activity']))
+    most_characters_text = 'Most characters in Activity: {} (characters: {})\n'.format(
+        most_characters['activity'], len(most_characters['activity']))
+
+    return most_participants_text + duplicate_activities_text + most_characters_text
+
+
+def generate_markdown_file(num_activities):
+    activities = get_activity_suggestions(num_activities)
+
+    # Create Markdown content
+    markdown_content = generate_table(activities) + '\n' + generate_insights(activities)
+
+    # Write Markdown file
+    with open('activity_suggestions.md', 'w') as f:
+        f.write(markdown_content)
+
+
+# Example usage with 10 activity suggestions
+generate_markdown_file(10)
